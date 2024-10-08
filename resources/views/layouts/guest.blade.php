88dd7a5eac26f54d9fe8e241e853f4f7f2276f38
JD Singh <jagdeepsingh@founderandlightning.com>
2024-10-08 11:29:08 +0530

    Make fluxible adjustments in guest

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,31 +1,50 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png" />
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
+        <!-- Styles -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @fluxStyles
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/" wire:navigate>
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+    <body
+        class="font-sans antialiased
+            {{-- text-gray-900 dark:text-gray-100 --}}
+            {{-- bg-gray-50 dark:bg-gray-900 --}}
+            bg-gray-50 text-black/50
+            dark:bg-black dark:text-white/50
+            h-full">
+        <flux:header container class="">
+            <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
+            <flux:brand
+                href="{{ route('homepage') }}"
+                wire:navigate
+                name="A!"
+                logo="{{ asset('assets/altron.png') }}"
+                class="max-lg:hidden" />
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <flux:spacer />
+            <livewire:welcome.navigation />
+        </flux:header>
+        <flux:main>
+            <main class="">
                 {{ $slot }}
-            </div>
-        </div>
+            </main>
+        </flux:main>
+        <flux:spacer />
+        <flux:footer class="text-center text-sm text-black dark:text-white/70">
+            Altron {{ date('Y') }}
+        </flux:footer>
         <x-dimensions />
+        @fluxScripts
     </body>
 </html>
