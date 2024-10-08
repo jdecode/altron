41a04ac692a9b9027c5a9cd3564d2e1f3c40994d
JD Singh <jagdeepsingh@founderandlightning.com>
2024-10-08 11:20:24 +0530

    Use auth + `get` homepage

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('homepage');
+Route::get('/', function () {
+    return view('pages.homepage');
+})->name('homepage');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
@@ -12,4 +14,4 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__ . '/user-auth.php';
+require __DIR__ . '/auth.php';
