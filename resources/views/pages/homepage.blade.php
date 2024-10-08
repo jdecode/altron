73ec632cefb43c9f18cc10ba1fec842b9b165e4b
JD Singh <jagdeepsingh@founderandlightning.com>
2024-10-08 11:23:07 +0530

    Create pages:  - Homepage  - Register  - Login

---
+++ resources/views/pages/homepage.blade.php
@@ -0,0 +1,10 @@
+<x-guest-layout>
+    <div id="screenshot-container" class="relative flex w-full flex-1 justify-center">
+        <img
+            src="{{ asset('assets/altron-text.png') }}"
+            alt="Altron | The boilerplate"
+            title="Altron | The boilerplate"
+            class="flex-1 rounded-[10px]  drop-shadow-[0px_4px_34px_rgba(0,0,0,0.25)] dark:block max-w-80"
+        />
+    </div>
+</x-guest-layout>
---
+++ resources/views/pages/register.blade.php
@@ -0,0 +1,3 @@
+<x-guest-layout>
+    <livewire:user.register />
+</x-guest-layout>
