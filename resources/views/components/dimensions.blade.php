d2dbb44f8945c5b375bd7f15043a6d7379ec55e9
JD Singh <jagdeepsingh@founderandlightning.com>
2024-10-08 09:17:43 +0530

    Dimension-viewer

---
+++ resources/views/components/dimensions.blade.php
@@ -0,0 +1,31 @@
+@if(app()->environment('local'))
+    <div
+        class="fixed z-50 left-0 bottom-0
+            bg-white/5 text-white
+            p-4 px-8
+            backdrop-blur-sm
+            text-lg font-bold
+            transition-all duration-100
+            rounded-2xl
+            border-orange-600 border-2"
+        title="Screen size indicator">
+    <span class="sm:hidden">
+        default &lt;640px
+    </span>
+        <span class="hidden sm:block md:hidden">
+        sm &lt;768px
+    </span>
+        <span class="hidden md:block lg:hidden">
+        md &lt;1024px
+    </span>
+        <span class="hidden lg:block xl:hidden">
+        lg &lt;1280px
+    </span>
+        <span class="hidden xl:block 2xl:hidden">
+        xl &lt;1536px
+    </span>
+        <span class="hidden 2xl:block">
+        2xl &gt;1536px
+    </span>
+    </div>
+@endif
