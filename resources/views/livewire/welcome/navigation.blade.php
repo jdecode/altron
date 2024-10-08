4743482b2d0f2fdb2f7cafb2e87ae4a56d55c73d
JD Singh <jagdeepsingh@founderandlightning.com>
2024-10-08 11:23:54 +0530

    Navigate over-the-wire

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -2,6 +2,7 @@
     @auth
         <a
             href="{{ url('/dashboard') }}"
+            wire:navigate
             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
         >
             Dashboard
@@ -9,6 +10,7 @@
     @else
         <a
             href="{{ route('login') }}"
+            wire:navigate
             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
         >
             Log in
@@ -17,6 +19,7 @@
         @if (Route::has('register'))
             <a
                 href="{{ route('register') }}"
+                wire:navigate
                 class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
             >
                 Register
